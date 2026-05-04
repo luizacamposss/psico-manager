bb03bc79f4ac9ccf8eadbeca7ebba4cc1ae1fa4b
Malu <mluizafields@gmail.com>
2026-05-04 20:48:25 +0000

    primeira de muitas

--- server.js
+++ server.js
@@ -77,6 +77,8 @@ app.get("/dashboard/receitas", (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log("Servidor rodando em http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
