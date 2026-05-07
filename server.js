68386d1572c63c9b0419ec4f28b76d37137c8b83
Malu <mluizafields@gmail.com>
2026-05-07 16:57:14 +0000

    corrige conexao mysql railway

--- server.js
+++ server.js
@@ -10,10 +10,11 @@ app.use(express.static("public"));
 
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "psico_manager"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
 db.connect(err => {
